e14e81e766d3e1b5f654fd44afdf6eebcf2f7e53
renzon <renzon@gmail.com>
2017-01-07 15:07:01 -0200

    Hanoi recursivo

---
+++ recursao/hanoi_rec.py
@@ -0,0 +1,22 @@
+def hanoi(n, orig='A', aux='B', dest='C'):
+    """Recursive Solution to Hanoi Tower
+    t(n) = 1 + 2(t(n-1))
+    t(n) = 1 + 2 + 4(t(n-2))
+    t(n) = 1 + 2 + 4 + 8(t(n-3))
+    t(n) = 1 + 2 + 4 + 8 + 16(t(n-4))
+    t(n) = 1 + 2 + 4 + 8 + 16 ... 2**n
+    2(t(n)) = 2 + 4 + 8 + 16 + 32 ... 2**(n+1)
+
+    t(n)=2**(n+1)-1 => O(2**n)
+
+    m(n) = 1 + m(n-1) => O(n)
+    """
+    if n >= 1:
+        hanoi(n - 1, orig, dest, aux)
+        print('{} -> {} : {}'.format(orig, dest, n))
+        hanoi(n - 1, aux, orig, dest)
+
+
+for i in range(1, 4):
+    print('####### Hanoi %s' % i)
+    hanoi(i)
