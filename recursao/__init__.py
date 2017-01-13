44438dc01a14f9bed8310b67eb7289ce397c6ecb
renzon <renzon@gmail.com>
2017-01-13 08:54:53 -0200

    Removendo elementos repetidos de uma lista

--- recursao/__init__.py
+++ recursao/__init__.py
@@ -1,2 +1,19 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
+def dedup(lst):
+    """Remove duplicates from lst
+
+    :param lst: a list
+    :return: new list without duplicated elements
+
+    linear for time and space
+    """
+    result = []
+    repeated = set()
+
+    for ele in lst:
+        if ele not in repeated:
+            result.append(ele)
+            repeated.add(ele)
+    return result
+
+
+print(dedup(['banana', 'banana', 'banana', 'abacaxi', 'caqui']))
